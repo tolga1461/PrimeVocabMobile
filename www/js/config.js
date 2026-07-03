b662fdcc54ea66baca6335e21ba7ddb11f02fc46
tolga1461 <fahrenheit_-18@hotmail.com>
2026-07-04 02:44:31 +0300

    [build] cap sync

--- www/js/config.js
+++ www/js/config.js
@@ -2,7 +2,7 @@
 window.CONFIG = {
     // Replace this with your Google OAuth Client ID for Web Applications from Google Cloud Console.
     // E.g., "1234567890-abc123xyz.apps.googleusercontent.com"
-    GOOGLE_CLIENT_ID: "", // Let user fill this in for sync
+    GOOGLE_CLIENT_ID: "123876704092-eo19nu69jggubqq158s7mkfi5o4n9rtu.apps.googleusercontent.com",
     
     // Authorization Scopes needed for Google Drive appDataFolder access
     GOOGLE_SCOPES: "https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
